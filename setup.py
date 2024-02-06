e26b4deaf94b5b70c30bf09bf5b0828ea522780e
milosnowcat <ramiro.alvarez@rahcode.com>
2024-02-05 18:10:15 -0600

    long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='ShieldCipher',
     version='1.0.0',
     description='Python library for cybersecurity',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='RAH Code',
     author_email='contacto@rahcode.com',
     url='https://rah-code-dev.github.io/ShieldCipher',
